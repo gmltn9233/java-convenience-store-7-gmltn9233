7c0118e6278b8220755e0f1805c2a9a7ea0270c1
이희수 <hslee9234@naver.com>
2024-11-11 04:22:30 +0900

    refactor: Application

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,21 +1,12 @@
 package store;
 
-import java.io.IOException;
 import store.common.config.StoreConfig;
-import store.common.exception.ErrorMessage;
 import store.controller.StoreController;
-import store.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-            StoreConfig storeConfig = new StoreConfig();
-            StoreController storeController = storeConfig.provideStoreController();
-            storeController.salesStart();
-        } catch (IOException e) {
-            OutputView.printError(ErrorMessage.READ_FILE_ERROR.getMessage());
-        } catch (Exception e) {
-            OutputView.printError(ErrorMessage.UNEXPECTED_ERROR.getMessage());
-        }
+        StoreConfig storeConfig = new StoreConfig();
+        StoreController storeController = storeConfig.provideStoreController();
+        storeController.salesStart();
     }
 }
--- src/main/java/store/common/config/StoreConfig.java
+++ src/main/java/store/common/config/StoreConfig.java
@@ -3,17 +3,23 @@ package store.common.config;
 import java.io.IOException;
 import store.common.Loader.PromotionLoader;
 import store.common.Loader.StoreLoader;
+import store.common.exception.ErrorMessage;
 import store.controller.StoreController;
 import store.domain.store.ConvenienceStore;
 import store.service.StoreService;
+import store.view.OutputView;
 
 public class StoreConfig {
-    public StoreController provideStoreController() throws IOException {
-        PromotionLoader promotionLoader = new PromotionLoader();
-        StoreLoader storeLoader = new StoreLoader(promotionLoader);
-        ConvenienceStore convenienceStore = storeLoader.createConvenienceStore();
-        StoreService storeService = new StoreService(convenienceStore);
-
-        return new StoreController(storeService);
+    public StoreController provideStoreController() {
+        try {
+            PromotionLoader promotionLoader = new PromotionLoader();
+            StoreLoader storeLoader = new StoreLoader(promotionLoader);
+            ConvenienceStore convenienceStore = storeLoader.createConvenienceStore();
+            StoreService storeService = new StoreService(convenienceStore);
+            return new StoreController(storeService);
+        } catch (IOException e) {
+            OutputView.printError(ErrorMessage.READ_FILE_ERROR.getMessage());
+            return null;
+        }
     }
 }
