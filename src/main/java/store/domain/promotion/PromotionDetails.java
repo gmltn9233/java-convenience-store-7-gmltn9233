a6a5bccec75c5b62c5bad441a55a992a2e9067f5
이희수 <hslee9234@naver.com>
2024-11-11 02:01:51 +0900

    feat: PromotionDetails 클래스 구현

---
+++ src/main/java/store/domain/promotion/PromotionDetails.java
@@ -0,0 +1,11 @@
+package store.domain.promotion;
+
+public class PromotionDetails {
+    private final int buy;
+    private final int bonus;
+
+    public PromotionDetails(int buy, int bonus) {
+        this.buy = buy;
+        this.bonus = bonus;
+    }
+}
