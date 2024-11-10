8cc24721596bb5b967ec5335e42f2907afc5d859
이희수 <hslee9234@naver.com>
2024-11-11 04:14:44 +0900

    refactor: Application
    try catch문으로 변경

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -2,12 +2,20 @@ package store;
 
 import java.io.IOException;
 import store.common.config.StoreConfig;
+import store.common.exception.ErrorMessage;
 import store.controller.StoreController;
+import store.view.OutputView;
 
 public class Application {
-    public static void main(String[] args) throws IOException {
-        StoreConfig storeConfig = new StoreConfig();
-        StoreController storeController = storeConfig.provideStoreController();
-        storeController.salesStart();
+    public static void main(String[] args) {
+        try {
+            StoreConfig storeConfig = new StoreConfig();
+            StoreController storeController = storeConfig.provideStoreController();
+            storeController.salesStart();
+        } catch (IOException e) {
+            OutputView.printError(ErrorMessage.READ_FILE_ERROR.getMessage());
+        } catch (Exception e) {
+            OutputView.printError(ErrorMessage.UNEXPECTED_ERROR.getMessage());
+        }
     }
 }
--- src/main/java/store/common/exception/ErrorMessage.java
+++ src/main/java/store/common/exception/ErrorMessage.java
@@ -7,14 +7,17 @@ public enum ErrorMessage {
     QUANTITY_MUST_BE_NUMBER("주문 수량은 숫자형식이여야 합니다."),
     QUANTITY_MUST_BE_POSITIVE_NUMBER("주문 수량은 0보다 커야 합니다."),
     INVALID_RESPONSE("응답은 Y 혹은 N으로 이루어져야 합니다."),
-    INVALID_QUANTITY("재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+    INVALID_QUANTITY("재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요."),
+    READ_FILE_ERROR("MD 파일을 읽는 과정에서 에러가 발생했습니다."),
+    UNEXPECTED_ERROR("예상치 못한 에러가 발생했습니다.");
 
     private final String message;
-    ErrorMessage(String message){
+
+    ErrorMessage(String message) {
         this.message = message;
     }
 
-    public String getMessage(){
+    public String getMessage() {
         return this.message;
     }
 
