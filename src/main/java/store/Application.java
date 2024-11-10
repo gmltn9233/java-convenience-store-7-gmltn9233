a033a6d130189b95468128e54e128d5f932d2a4c
이희수 <hslee9234@naver.com>
2024-11-11 03:53:50 +0900

    feat: Application

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,13 @@
 package store;
 
+import java.io.IOException;
+import store.common.config.StoreConfig;
+import store.controller.StoreController;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args) throws IOException {
+        StoreConfig storeConfig = new StoreConfig();
+        StoreController storeController = storeConfig.provideStoreController();
+        storeController.salesStart();
     }
 }
