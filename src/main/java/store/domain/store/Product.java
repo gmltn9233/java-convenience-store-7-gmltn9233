2b5b376429cbb223b1cf4f3ca41ea4a05e54619b
이희수 <hslee9234@naver.com>
2024-11-11 01:32:54 +0900

    feat: Product 클래스 구현

---
+++ src/main/java/store/domain/store/Product.java
@@ -0,0 +1,38 @@
+package store.domain.store;
+
+import java.util.Objects;
+
+public class Product {
+    private final String name;
+    private final int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return price == product.price && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
+}
---
+++ src/main/java/store/service/StoreService.java
@@ -0,0 +1,80 @@
+package store.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import store.common.exception.ErrorMessage;
+import store.common.exception.InvalidOrderException;
+import store.domain.order.Order;
+import store.domain.order.Orders;
+import store.domain.order.PromotionOrder;
+import store.domain.order.Receipt;
+import store.domain.promotion.Promotion;
+import store.domain.promotion.PromotionManager;
+import store.domain.store.ConvenienceStore;
+import store.domain.store.Product;
+import store.domain.store.StoreInventory;
+import store.dto.request.OrdersRequest;
+import store.dto.response.ReceiptResponse;
+import store.dto.response.ReceiptResponse.OrderDetail;
+import store.dto.response.StoreResponse;
+
+public class StoreService {
+
+    private final ConvenienceStore convenienceStore;
+
+    public StoreService(ConvenienceStore convenienceStore){
+        this.convenienceStore = convenienceStore;
+    }
+    public Orders generateOrders(OrdersRequest ordersRequest) {
+        StoreInventory storeInventory = convenienceStore.getStoreInventory();
+
+        List<Order> orders = ordersRequest.orders().entrySet().stream()
+                .map(entry -> createOrder(storeInventory, entry.getKey(), entry.getValue()))
+                .collect(Collectors.toList());
+
+        return new Orders(orders);
+    }
+
+    private Order createOrder(StoreInventory storeInventory, String productName, int quantity) {
+        Product product = storeInventory.findProductByName(productName);
+
+        if (product == null) {
+            throw new InvalidOrderException(ErrorMessage.INVALID_PRODUCT.getMessage());
+        }
+
+        return new Order(product, quantity);
+    }
+
+    public List<StoreResponse> generateStoreResponses() {
+        StoreInventory storeInventory = convenienceStore.getStoreInventory();
+        PromotionManager promotionManager = convenienceStore.getPromotionManager();
+        return storeInventory.getProductInventoryDetails(promotionManager);
+    }
+
+
+    public ReceiptResponse createReceiptResponse(Receipt receipt) {
+        List<OrderDetail> productOrderDetails = receipt.getProductOrders().toOrderDetails();
+        List<OrderDetail> giftOrderDetails = receipt.getGiftOrders().toGiftOrderDetails();
+
+        int totalQuantity = receipt.getProductOrders().getTotalQuantity();
+        int totalPurchaseAmount = receipt.getProductOrders().getTotalPurchaseAmount();
+        int promotionalDiscount = receipt.getGiftOrders().getPromotionalDiscount();
+        int membershipDiscount = receipt.calculateMembershipDiscount();
+
+        return ReceiptResponse.from(
+                productOrderDetails,
+                giftOrderDetails,
+                totalQuantity,
+                totalPurchaseAmount,
+                promotionalDiscount,
+                membershipDiscount
+        );
+    }
+
+
+    public Receipt generateReceipt(Orders orders){
+        return convenienceStore.purchaseProducts(orders);
+    }
+}
